73ce36e3d209e22d71b098927e7f93bcfffde5c0
SlackxRA1D <53799083+SlackxRA1D@users.noreply.github.com>
2019-10-01 13:22:12 -0300

    ta aqui

--- SOP.java
+++ SOP.java
@@ -6,8 +6,10 @@ public class SOP {
 
    
     public static void main(String[] args) {
-     
-        
+       for (int i = 0; i <=1000000; i++) {
+        int aleatorio = (int) (Math.random()*19) +1;
+        System.out.println("numero:"+ aleatorio);
+        }           
         
         Jogador j1 = new Jogador();
         
